8e0a52cb4daa6b83f3e7bc59883dd95a82e1149b
iraivg <74787633+iraivg@users.noreply.github.com>
2020-11-27 13:27:53 +0200

    Add files via upload

---
+++ Fadi.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fadi;
+
+/**
+ *
+ * @author Len_Z50-70
+ */
+public class Fadi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
